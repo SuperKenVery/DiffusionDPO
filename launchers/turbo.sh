830f7bbecd551827bbf56dd830b175d2aab307a6
ken <1610057945@qq.com>
2025-10-12 13:49:21 +0000

    Update accelerate version

--- launchers/turbo.sh
+++ launchers/turbo.sh
@@ -1,6 +1,7 @@
 export MODEL_NAME="stabilityai/sdxl-turbo"
 export VAE="madebyollin/sdxl-vae-fp16-fix"
-export DATASET_NAME="yuvalkirstain/pickapic_v2"
+# export DATASET_NAME="yuvalkirstain/pickapic_v2"
+export DATASET_NAME="BraceZHY/pickapic_v2_300k"
 
 
 # Effective BS will be (N_GPU * train_batch_size * gradient_accumulation_steps)
@@ -10,15 +11,14 @@ accelerate launch train.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --pretrained_vae_model_name_or_path=$VAE \
   --dataset_name=$DATASET_NAME \
-  --train_batch_size=1 \
+  --train_batch_size=4 \
   --dataloader_num_workers=16 \
   --gradient_accumulation_steps=128 \
   --max_train_steps=2000 \
   --lr_scheduler="constant_with_warmup" --lr_warmup_steps=200 \
   --learning_rate=1e-8 --scale_lr \
-  --cache_dir="/export/share/datasets/vision_language/pick_a_pic_v2/" \
-  --checkpointing_steps 200 \
+  --checkpointing_steps 20 \
   --beta_dpo 5000 \
-   --sdxl --resolution 512 --proportion_empty_prompts 0 \
-  --output_dir="turbo-trial-beta5k-lr1e-8-warmup200"
+  --sdxl --resolution 512 --proportion_empty_prompts 0 \
+  --output_dir="trainings/reproduce-turbo-pickapic-update-packages"
 
--- train.py
+++ train.py
@@ -1185,6 +1185,12 @@ def main():
                         logger.info(f"Saved state to {save_path}")
                         logger.info("Pretty sure saving/loading is fixed but proceed cautiously")
 
+                        delete_step = global_step - args.checkpointing_steps
+                        delete_path = os.path.join(args.output_dir, f"checkpoint-{delete_step}")
+                        if os.path.exists(delete_path):
+                            logger.info(f"Deleting old checkpoint {delete_path}")
+                            shutil.rmtree(delete_path)
+
             logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
             if args.train_method == 'dpo':
                 logs["implicit_acc"] = avg_acc
